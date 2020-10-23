02f9b799e003c9a962da1401fafdd540ff14ed09
Shashwat <shashwatagrawal473@gmail.com>
2020-10-23 11:14:14 +0530

    MultipleProducts

--- _02_MyEcommerceCart/shop/views.py
+++ _02_MyEcommerceCart/shop/views.py
@@ -4,11 +4,19 @@ from math import ceil
 from .models import Product
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
-    # print(products)
-    n = len(products)
-    nSlides = n//4 - ceil((n/4) - (n//4) )
-    params = {'no_of_Slides': nSlides,'range': range(1, nSlides),  'product':products}
+    # products = Product.objects.all()
+    # # print(products)
+    
+    # params = {'no_of_Slides': nSlides,'range': range(1, nSlides),  'product':products}
+    allProds = []
+    catprods = Product.objects.values('category', 'id', 'desc')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n//4 + ceil((n/4) - (n//4) )
+        allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
 
 
