8cb9f161ec19f17855b4320b2ba3ce3abe9a6f3f
Shashwat <shashwatagrawal473@gmail.com>
2020-11-07 18:58:53 +0530

    Initial Commit

---
+++ Shashwat/home/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from home.models import Contact
+
+# Register your models here.
+
+admin.site.register(Contact)
+
+
---
+++ Shashwat/home/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+# class Search(models.Model):
+#     searches = models.CharField(max_length=80)
+#     timeStamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+class Contact(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=13)
+    email = models.CharField(max_length=100)
+    content = models.TextField()
+    timeStamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return "Message from " + "- " + self.name + "-" "Email"+ "- " + self.email + self.content[0:20] + "..."
